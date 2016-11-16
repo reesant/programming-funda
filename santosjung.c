2f7954c3300ba48c54be9829b3c87d0285b3eb58
santos <santos.basnet23@gmail.com>
2016-11-16 10:36:23 +0545

    Add files via upload

---
+++ santosjung.c
@@ -0,0 +1,35 @@
+/*
+ author : santosh jung basnet
+subject : programing fundamental
+roll no .:18
+labsheet no. : 2
+program : write a program to print sum of two nos. asked from users.
+date: 16/11/2016
+*/
+
+# include <stdio.h>
+# include <conio.h>
+int main (){
+
+
+int a,b,sum;
+
+printf ("enter the no.");
+scanf("%d",&a);
+printf ("enter another no.");
+scanf ("%d",&b);
+sum = a+b;
+printf ("the sum of two no. is:%d",sum);
+
+return 0;
+
+
+
+
+
+
+
+
+
+
+}
